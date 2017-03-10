8835250736202437fbc7546e222e387916987e8c
odahcam <luizfilipe2557@gmail.com>
2017-03-10 19:24:23 -0300

    updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 // Load plugins
 var gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
-  minifycss = require('gulp-minify-css'),
+  cleanCSS = require('gulp-clean-css'),
   jshint = require('gulp-jshint'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   clean = require('gulp-clean'),
   notify = require('gulp-notify'),
-  cache = require('gulp-cache'),
-  livereload = require('gulp-livereload'),
-  server = lr();
+  cache = require('gulp-cache');
+
+var sourceMaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var remember = require('gulp-remember');
 
 // Styles
 gulp.task('styles', function () {
   return gulp.src('src/css/*.less')
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 10', 'ie 11', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(livereload(server))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(notify({ message: 'Styles task complete!' }));
+    .pipe(sourceMaps.init())
+    .pipe(cache('less'))
+    .pipe(less())
+    .pipe(remember('less'))
+    .pipe(sourceMaps.write('dist/css/'))
+    .pipe(gulp.dest('dist/css/'));
 });
 
 // Scripts
@@ -31,14 +32,13 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('dist/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(livereload(server))
     .pipe(gulp.dest('dist/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Clean
 gulp.task('clean', function () {
-  return gulp.src(['dist/styles', 'dist/js'], { read: false })
+  return gulp.src('dist/', { read: false })
     .pipe(clean());
 });
 
@@ -50,24 +50,10 @@ gulp.task('default', ['clean'], function () {
 // Watch
 gulp.task('watch', function () {
 
-  // Listen on port 35729
-  server.listen(35729, function (err) {
-    if (err) {
-      return console.log(err)
-    };
-
     // Watch .scss files
-    gulp.watch('src/styles/**/*.less', function (event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-      gulp.run('styles');
-    });
+    gulp.watch('src/css/*.less', 'styles');
 
     // Watch .js files
-    gulp.watch('src/js/**/*.js', function (event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-      gulp.run('scripts');
-    });
-
-  });
+    gulp.watch('src/js/*.js', 'scripts');
 
 });
